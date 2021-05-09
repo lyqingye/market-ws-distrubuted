4a4ef633605c7e5a4a740381ae42456c97d144ba
lyqingye <1316515719@qq.com>
2021-05-09 22:04:58 +0800

    feat: 初步引入hazelcast集群启动器

---
+++ core/spi/hazelcast-cluster-bootstrap/src/main/java/com/tqxd/jys/core/spi/HazelcastBootstrap.java
@@ -0,0 +1,12 @@
+package com.tqxd.jys.core.spi;
+
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Verticle;
+
+public class HazelcastBootstrap implements Bootstrap {
+
+  @Override
+  public void start(Verticle verticle, DeploymentOptions options) {
+
+  }
+}
